f0e5500e128b5080cfb6515b471831f78af742e6
Nico De Witte <nico.dewitte@vives.be>
2019-03-21 14:28:42 +0100

    Refactor to allow user to enter his/her name

--- src/main/java/be/vives/oop/javafx/scene_switching/LoginController.java
+++ src/main/java/be/vives/oop/javafx/scene_switching/LoginController.java
@@ -5,17 +5,17 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
     
-    @FXML
-    private Label label;
+    @FXML private TextField username;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void loadApplicationScene(ActionEvent event) {
+        System.out.println("Loading Application Controller");
+        String name = username.getText();
+        System.out.println("User entered name = " + name);
     }
     
     @Override
