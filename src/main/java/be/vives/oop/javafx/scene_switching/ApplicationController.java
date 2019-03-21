3f7653c34b37a5613ca2be886bbfa464e236f4a7
Nico De Witte <nico.dewitte@vives.be>
2019-03-21 14:32:53 +0100

    Switching scenes without passing data

---
+++ src/main/java/be/vives/oop/javafx/scene_switching/ApplicationController.java
@@ -0,0 +1,17 @@
+package be.vives.oop.javafx.scene_switching;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class ApplicationController implements Initializable {
+    
+    @FXML private Label username;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
--- src/main/java/be/vives/oop/javafx/scene_switching/LoginController.java
+++ src/main/java/be/vives/oop/javafx/scene_switching/LoginController.java
@@ -1,21 +1,37 @@
 package be.vives.oop.javafx.scene_switching;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 
 public class LoginController implements Initializable {
-    
-    @FXML private TextField username;
-    
-    @FXML
-    private void loadApplicationScene(ActionEvent event) {
+
+  @FXML
+  private TextField username;
+
+  @FXML
+  private void loadApplicationScene(ActionEvent event) throws IOException {
         System.out.println("Loading Application Controller");
         String name = username.getText();
         System.out.println("User entered name = " + name);
+
+        // Need stage to change scenes
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ApplicationScene.fxml"));
+        Scene appScene = new Scene(root);
+
+        stage.hide(); //optional
+        stage.setScene(appScene);
+        stage.show();   
     }
     
     @Override
